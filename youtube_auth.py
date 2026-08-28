73dfbd01f76f49d78e91e22ebf322cfb26c06008
petrinofamily1997-tech <petrinofamily1997@gmail.com>
2026-08-28 21:19:41 +0300

    Create youtube_auth.py

---
+++ youtube_auth.py
@@ -0,0 +1,18 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = [
+    "https://www.googleapis.com/auth/youtube.upload"
+]
+
+flow = InstalledAppFlow.from_client_secrets_file(
+    "client_secret.json",
+    SCOPES
+)
+
+credentials = flow.run_local_server(port=8080)
+
+print("\nACCESS TOKEN:")
+print(credentials.token)
+
+print("\nREFRESH TOKEN:")
+print(credentials.refresh_token)
